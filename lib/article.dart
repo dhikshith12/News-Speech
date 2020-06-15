cfb0ac85a476cca7e3087d7ae03fa66d3999fdd0
dhikshith12 <dhikshith12@users.noreply.github.com>
2020-06-15 16:38:56 +0530

    Listen To News On The Go

---
+++ lib/article.dart
@@ -0,0 +1,11 @@
+class Article{
+ String title;
+ String author;
+ String description;
+ DateTime publishedAt;
+ String content;
+ String urlToImage;
+ 
+ Article({this.author,this.content,this.description,this.publishedAt,this.title,this.urlToImage});
+ 
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'article.dart';
+
+class NewsApp extends StatefulWidget {
+  @override
+  _NewsAppState createState() => _NewsAppState();
+}
+
+class _NewsAppState extends State<NewsApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
